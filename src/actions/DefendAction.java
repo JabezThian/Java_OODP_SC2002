767a74c147fadb42c181be8c86aa16d431d07ff4
jabezthian <thian.jabez@gmail.com>
2026-04-09 01:22:52 +0800

    actions - updated

---
+++ src/actions/DefendAction.java
@@ -0,0 +1,17 @@
+package actions;
+
+import entities.Combatant;
+import effects.DefenseBuffEffect;
+import java.util.List;
+
+public class DefendAction implements Action {
+    @Override
+    public void execute(Combatant performer, List<Combatant> targets) {
+        // Apply +10 Defense for 1 round
+        performer.addEffect(new DefenseBuffEffect(10, 1));
+        System.out.println(">> " + performer.getName() + " raises their shield! Defense increased by 10 for this round.");
+    }
+
+    @Override
+    public String getName() { return "Defend"; }
+}
