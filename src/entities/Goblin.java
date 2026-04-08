704ee2789b67b7190b57495b74f0d8cf09331a4c
jabezthian <thian.jabez@gmail.com>
2026-04-08 17:00:07 +0800

    Combatant

---
+++ src/entities/Goblin.java
@@ -0,0 +1,6 @@
+public class Goblin extends Enemy {
+    public Goblin(String name) {
+
+        super(name, 55, 35, 15, 25);
+    }
+}
---
+++ src/entities/Player.java
@@ -0,0 +1,9 @@
+public abstract class Player extends Combatant {
+    protected int specialCooldown = 0;
+    protected Action specialSkill;
+
+    public Player(String name, int health, int attack, int defense, int speed, Action skill) {
+        super(name, health, attack, defense, speed);
+        this.specialSkill = skill;
+    }
+}
