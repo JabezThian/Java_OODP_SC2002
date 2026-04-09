5f5ec65b1fab8ee752bca6e070164213a995cec4
Zhang Haoyuan <c250167@e.ntu.edu.sg>
2026-04-09 17:13:05 +0800

    Update BasicAttack.java

--- src/actions/BasicAttack.java
+++ src/actions/BasicAttack.java
@@ -1,21 +1,14 @@
 package actions;
 import entities.Combatant;
 import java.util.List;
+import core.DamageCalculator;
 
 public class BasicAttack implements Action {
+    @Override
     public void execute(Combatant performer, List<Combatant> targets) {
+        if (targets == null || targets.isEmpty()) return;
         Combatant target = targets.get(0);
-
-        int atk = performer.getAttack().getValue();
-        int def = target.getTotalDefense();
-
-        int damage = Math.max(0, atk - def);
-        target.getHealth().takeDamage(damage);
-
-        // Display status
-        System.out.println(performer.getName() + " struck (" + atk + " ATK - " + def + " DEF) = " + damage + " DMG to " + target.getName());
-        System.out.println(">> " + target.getName() + " now has " + target.getHealth().getValue() + " HP left.");
-        System.out.println();
+        new DamageCalculator(performer, target).executeDamage();
     }
     public String getName() { return "Basic Attack"; }
 }
