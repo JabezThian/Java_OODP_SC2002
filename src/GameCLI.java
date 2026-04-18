7064c601906deb117eafb76112117102f118ee5e
IEatDonuts <darkyblizzard@gmail.com>
2026-04-18 12:46:20 +0800

    GameCLI

--- src/GameCLI.java
+++ src/GameCLI.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import core.BattleEngine;
 
-public class Main {
+public class GameCLI {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         BattleEngine engine = new BattleEngine();
